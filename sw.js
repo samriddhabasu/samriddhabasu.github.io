a0910677b15f22ea5d758bfa1dca1fa4c8523e51
Samriddha Basu <samriddhabasu@gmail.com>
2017-03-04 02:10:30 +0530

    Basic support for Progressive Web App

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('samriddhabasu.github.io').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.html?launcher=true',
+        '/css/main.css',
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
